5b01bea7c0cde3f5f7e729f69a930844800e688c
xSHAFTx <cameron@thenearsky.com>
2014-02-06 14:19:39 -0600

    Control Structures 1 exercise 3

--- php/if.php
+++ php/if.php
@@ -3,41 +3,50 @@
 $a = 5;
 $b = 10;
 $c = '10';
+
+// Shorten the next 2 statements into an if/else
 if ($a < $b) {
 	// output the appropriate result
 	echo "$a is less than $b\n";
 }
 
-if ($b > $a) {
+else {
 	// output the appropriate result
-	echo "$b is greater than $a\n";
+	echo "$a is greater than $b\n";
 }
 
+// Shorten the next 2 statements into an if/else
 if ($b >= $c) {
 	// output the appropriate result
 	echo "$b is greater than or equal to $c\n";
 }
 
-if ($b <= $c) {
+else {
 	// output the appropriate result
 	echo "$b is less than or equal to $c\n";
 }
 
-if ($b == $c) {
+//combine the next 4 conditionals into
+//one if/else/elseif block that checks in order for:
+//identical, equal, not identical, not equal
+if ($b === $c) {
+	// output the appropriate result
+	echo "$b is identical to $c\n";
+}
+
+elseif ($b == $c) {
 	// output the appropriate result
 	echo "$b is equal to $c\n";
 }
 
-if ($b === $c) {
-	echo "$b is identical to $c\n";
+elseif ($b !== $c) {
+	// output the appropriate result
+	echo "$b is not identical to $c\n";
 }
 
-// output the appropriate result
-if ($b != $c) {
+else {
+	// output the appropriate result
 	echo "$b is not equal to $c\n";
 }
 
-// output the appropriate result
-if ($b !== $c) {
-	echo "$b is not identical to $c\n";
-}
+?>
