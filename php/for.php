a223308ff0d7bb4291366f2259bf1362a64b2bd9
xSHAFTx <cameron@thenearsky.com>
2014-02-07 15:58:55 -0600

    Control Structures II Exercise 1 Step 4

--- php/for.php
+++ php/for.php
@@ -1,13 +1,22 @@
 <?php
 
 fwrite(STDOUT, "What number do you want to start with? ");
-$start = intval(fgets(STDIN));
+$start = rtrim(fgets(STDIN));
 fwrite(STDOUT, "What number do you want to end with? ");
-$end = intval(fgets(STDIN));
+$end = rtrim(fgets(STDIN));
 fwrite(STDOUT, "What increment do you want to use? ");
-$increase = intval(fgets(STDIN));
-if($increase == 0) {
+$increase = rtrim(fgets(STDIN));
+if($increase == "\n") {
 	$increase = 1;
 }
+
+if(is_numeric($start) && is_numeric($end) && is_numeric($increase)) {
+	for($i = $start; $i <= $end; $i += $increase) {
+		echo "$i\n";
+	}
+} else {
+	echo "You did not enter numbers!";
+}
+
 
 ?>
