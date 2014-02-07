99c3fa3ba2821382cff4e4d33f6fb7a7270c50a1
xSHAFTx <cameron@thenearsky.com>
2014-02-07 13:41:18 -0600

    highlow with  and

--- php/highlow.php
+++ php/highlow.php
@@ -1,38 +1,43 @@
 <?php
 
-//variables
-$answer = mt_rand(1, 1000000);
-$guess = '';
-$count = 0;
+if ($argc == 3) {
+	//variables
+	$answer = mt_rand($argv[1], $argv[2]);
+	$guess = '';
+	$count = 0;
 
-//computer guess guidelines
-$compLow = 1;
-$compHigh = 1000000;
+	//computer guess guidelines
+	$compLow = $argv[1];
+	$compHigh = $argv[2];
 
-//game
-while(TRUE) {
+	//game
+	while(TRUE) {
 
-	fwrite(STDOUT, "What is your guess? ");
+		fwrite(STDOUT, "What is your guess? ");
 
-	//computer's guess
-	$guess = mt_rand($compLow, $compHigh);
-	echo $guess . "\n";
+		//computer's guess
+		$guess = mt_rand($compLow, $compHigh);
+		echo $guess . "\n";
 
-	//check guess
-	if ($guess < $answer) {
-		echo "The number is higher then that\n";
-		$compLow = $guess + 1;
-		$count++;
-	} elseif ($guess > $answer) {
-		echo "The number is lower then that\n";
-		$compHigh = $guess - 1;
-		$count++;
-	} else {
-		echo "Thats it! Great guess!\n";
-		$count++;
-		echo "You guessed $count times.\n";
-		exit(0);
+		//check guess
+		if ($guess < $answer) {
+			echo "The number is higher then that\n";
+			$compLow = $guess + 1;
+			$count++;
+		} elseif ($guess > $answer) {
+			echo "The number is lower then that\n";
+			$compHigh = $guess - 1;
+			$count++;
+		} else {
+			echo "Thats it! Great guess!\n";
+			$count++;
+			echo "You guessed $count times.\n";
+			exit(0);
+		}
 	}
+} else {
+	echo "not enough arguments";
 }
+
 
 ?>
