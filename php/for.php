04f55f60098764174c3b2e457c8334e3b753ac2f
xSHAFTx <cameron@thenearsky.com>
2014-02-07 15:34:46 -0600

    Control Structures II Exercise 1 Step 3

--- php/for.php
+++ php/for.php
@@ -1,10 +1,13 @@
 <?php
 
 fwrite(STDOUT, "What number do you want to start with? ");
-$start = fgets(STDIN);
+$start = intval(fgets(STDIN));
 fwrite(STDOUT, "What number do you want to end with? ");
-$end = fgets(STDIN);
-fwrite(STDOUT, "What increment do you want to use? ")
-$increment = fgets(STDIN);
+$end = intval(fgets(STDIN));
+fwrite(STDOUT, "What increment do you want to use? ");
+$increase = intval(fgets(STDIN));
+if($increase == 0) {
+	$increase = 1;
+}
 
 ?>
