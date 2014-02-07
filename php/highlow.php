e5e3afd98dee9c19d4b97213234d3202c4a7f185
xSHAFTx <cameron@thenearsky.com>
2014-02-07 10:57:03 -0600

    highlow game final version

--- php/highlow.php
+++ php/highlow.php
@@ -2,16 +2,21 @@
 
 $answer = mt_rand(1, 100);
 $guess = '';
+$count = 0;
 
 while(TRUE) {
 	fwrite(STDOUT, "Guess? ");
 	$guess = fgets(STDIN);
 	if ($guess < $answer) {
 		echo "HIGHER\n";
+		$count++;
 	} elseif ($guess > $answer) {
 		echo "LOWER\n";
+		$count++;
 	} else {
 		echo "GOOD GUESS!\n";
+		$count++;
+		echo "$count\n";
 		exit(0);
 	}
 }
