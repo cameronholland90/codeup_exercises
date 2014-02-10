b6ed3d2e82771e586e12d4f7270c13ba65b6f08d
xSHAFTx <cameron@thenearsky.com>
2014-02-10 15:18:43 -0600

    added mt_rand() for testing

--- php/switch.php
+++ php/switch.php
@@ -5,7 +5,7 @@ date_default_timezone_set('America/Chicago');
 
 // Get Day of Week as number
 // 1 (for Monday) through 7 (for Sunday)
-$day_of_week = date('N');
+$day_of_week = mt_rand(1, 7);
 
 switch($day_of_week) {
     case 1:
