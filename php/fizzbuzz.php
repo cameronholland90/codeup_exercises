e75ad05dfb354414d4d1b0eb4b4396455696f68b
xSHAFTx <cameron@thenearsky.com>
2014-02-10 09:15:42 -0600

    fizzbuzz with option to change fizz and buzz values

--- php/fizzbuzz.php
+++ php/fizzbuzz.php
@@ -11,16 +11,32 @@ echo "It is a $fileType file!\n\n";
 // asks user for range to run in fizzbuzz
 fwrite(STDOUT, "What is your low number? ");
 $rangeLow = rtrim(fgets(STDIN));
-fwrite(STDOUT, "What is your high number?");
+fwrite(STDOUT, "What is your high number? ");
 $rangeHigh = rtrim(fgets(STDIN));
+
+// gives user option to change fizz and buzz values
+fwrite(STDOUT, "Would you like to change the values to 'Fizz' and 'Buzz'? ");
+$answer = rtrim(fgets(STDIN));
+if ($answer == 'yes' || $answer == 'y') {
+	fwrite(STDOUT, "What would you like to 'Fizz'? ");
+	$fizz = rtrim(fgets(STDIN));
+	fwrite(STDOUT, "What would you like to 'Buzz'? ");
+	$buzz = rtrim(fgets(STDIN));
+} 
+
+// sets fizz and buzz values if user does not want to change them
+else {
+	$fizz = 3;
+	$buzz = 5;
+}
 
 // loops through all numbers within range given by user
 for ($i = $rangeLow; $i <= $rangeHigh; $i++) { 
 
 	// checks if number is a mulitple of 3
-	if ($i % 3 == 0) {
+	if ($i % $fizz == 0) {
 		// checks if number is also a mulitple of 5
-		if ($i % 5 == 0) {
+		if ($i % $buzz == 0) {
 			echo "FizzBuzz\n";
 		} else {
 			echo "Fizz\n";
@@ -28,7 +44,7 @@ for ($i = $rangeLow; $i <= $rangeHigh; $i++) {
 	} 
 
 	// checks if number is a mulitple of 5
-	else if ($i % 5 == 0) {
+	else if ($i % $buzz == 0) {
 		echo "Buzz\n";
 	} 
 
