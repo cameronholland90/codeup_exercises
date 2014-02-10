dd2b145ac06cfdea47e60d9185d016100d9afd1d
xSHAFTx <cameron@thenearsky.com>
2014-02-10 11:59:08 -0600

    Control Structures II Exercise 3 Step 1

---
+++ php/foreach_2.php
@@ -0,0 +1,34 @@
+<?php
+
+$books = array(
+    'The Hobbit' => array(
+        'published' => 1937,
+        'author' => 'J. R. R. Tolkien',
+        'pages' => 310
+    ),
+    'Game of Thrones' => array(
+        'published' => 1996,
+        'author' => 'George R. R. Martin',
+        'pages' => 835
+    ),
+    'The Catcher in the Rye' => array(
+        'published' => 1951,
+        'author' => 'J. D. Salinger',
+        'pages' => 220
+    ),
+    'A Tale of Two Cities' => array(
+        'published' => 1859,
+        'author' => 'Charles Dickens',
+        'pages' => 544
+    )
+);
+
+foreach ($books as $title => $book) {
+	echo "{$title}:\n";
+	foreach ($book as $info => $value) {
+		echo "{$info} - $value\n";
+	}
+	echo "\n";
+}
+
+?>
