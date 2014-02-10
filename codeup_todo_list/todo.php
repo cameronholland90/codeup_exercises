726d14f2ef4c1b0b9d9080968c56f408184a1726
xSHAFTx <cameron@thenearsky.com>
2014-02-10 15:58:54 -0600

    origianl code for exercise

---
+++ codeup_todo_list/todo.php
@@ -0,0 +1,44 @@
+<?php
+
+// Create array to hold list of todo items
+$items = array();
+
+// The loop!
+do {
+    // Iterate through list items
+    foreach ($items as $key => $item) {
+        // Display each item and a newline
+        echo "[{$key}] {$item}\n";
+    }
+
+    // Show the menu options
+    echo '(N)ew item, (R)emove item, (Q)uit : ';
+
+    // Get the input from user
+    // Use trim() to remove whitespace and newlines
+    $input = trim(fgets(STDIN));
+
+    // Check for actionable input
+    if ($input == 'N') {
+        // Ask for entry
+        echo 'Enter item: ';
+        // Add entry to list array
+        $items[] = trim(fgets(STDIN));
+    } elseif ($input == 'R') {
+        // Remove which item?
+        echo 'Enter item number to remove: ';
+        // Get array key
+        $key = trim(fgets(STDIN));
+        // Remove from array
+        unset($items[$key]);
+    }
+// Exit when input is (Q)uit
+} while ($input != 'Q');
+
+// Say Goodbye!
+echo "Goodbye!\n";
+
+// Exit with 0 errors
+exit(0);
+
+?>
