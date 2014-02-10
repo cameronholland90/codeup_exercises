db2518a352abcbfceb26914733cea32fb6bb1f82
xSHAFTx <cameron@thenearsky.com>
2014-02-10 14:27:27 -0600

    Control Structures II Exercise 4 Step 2

--- php/break_continue.php
+++ php/break_continue.php
@@ -1,10 +1,17 @@
 <?php
 
+// for ($i=1; $i <= 100; $i++) { 
+// 	if ($i % 2 != 0) {
+// 		continue;
+// 	}
+// 	echo "$i\n";
+// }
+
 for ($i=1; $i <= 100; $i++) { 
-	if ($i % 2 != 0) {
-		continue;
-	}
 	echo "$i\n";
+	if($i == 10){
+		break;
+	}
 }
 
 ?>
