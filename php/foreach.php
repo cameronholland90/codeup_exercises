e27cd3d4b80d69557aa5466eec27a36d44143ff7
xSHAFTx <cameron@thenearsky.com>
2014-02-10 10:37:56 -0600

    Control Structures II Exercise 2 Step 2

--- php/foreach.php
+++ php/foreach.php
@@ -2,19 +2,25 @@
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", FALSE, (string) 11);
 
+// foreach ($things as $thing) {
+// 	if (is_int($thing)) {
+// 		echo "integer\n";
+// 	} elseif (is_float($thing)) {
+// 		echo "float\n";
+// 	} elseif (is_bool($thing)) {
+// 		echo "boolean\n";
+// 	} elseif (is_array($thing)) {
+// 		echo "array\n";
+// 	} elseif (is_null($thing)) {
+// 		echo "null\n";
+// 	} elseif (is_string($thing)) {
+// 		echo "string\n";
+// 	}
+// }
+
 foreach ($things as $thing) {
-	if (is_int($thing)) {
-		echo "integer\n";
-	} elseif (is_float($thing)) {
-		echo "float\n";
-	} elseif (is_bool($thing)) {
-		echo "boolean\n";
-	} elseif (is_array($thing)) {
-		echo "array\n";
-	} elseif (is_null($thing)) {
-		echo "null\n";
-	} elseif (is_string($thing)) {
-		echo "string\n";
+	if (is_scalar($thing)) {
+		echo "$thing\n";
 	}
 }
 
