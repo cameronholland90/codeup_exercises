fb5f49335b5d39008f80823e7c623dfaffd2783c
xSHAFTx <cameron@thenearsky.com>
2014-02-10 15:44:47 -0600

    running both day and month switch statments

--- php/switch.php
+++ php/switch.php
@@ -5,30 +5,30 @@ date_default_timezone_set('America/Chicago');
 
 // Get Day of Week as number
 // 1 (for Monday) through 7 (for Sunday)
-// $day_of_week = mt_rand(1, 7);
+$day_of_week = mt_rand(1, 7); // instead of random do date('N')
 
-// switch($day_of_week) {
-//     case 1:
-//         echo "Today is Monday.";
-//         break;
-//     case 2: 
-//         echo "Today is Tuesday.";
-//         break;
-//     case 3: 
-//         echo "Today is Wednesday.";
-//         break;
-//     case 4: 
-//         echo "Today is Thursday.";
-//         break;
-//     case 5: 
-//         echo "Today is Friday.";
-//         break;
-//     default:
-//     	echo "It is the Weekend";
-//     	break;
-// }
+switch($day_of_week) {
+    case 1:
+        echo "Today is Monday.";
+        break;
+    case 2: 
+        echo "Today is Tuesday.";
+        break;
+    case 3: 
+        echo "Today is Wednesday.";
+        break;
+    case 4: 
+        echo "Today is Thursday.";
+        break;
+    case 5: 
+        echo "Today is Friday.";
+        break;
+    default:
+    	echo "It is the Weekend";
+    	break;
+}
 
-$month_of_year = mt_rand(1, 12);
+$month_of_year = mt_rand(1, 12); // instead of random do date('m')
 
 switch($month_of_year) {
     case 1:
