2bab193d687c26867381b559601d4cb3b6faace1
xSHAFTx <cameron@thenearsky.com>
2014-02-10 09:01:32 -0600

    fizzbuzz with user input for range

--- php/fizzbuzz.php
+++ php/fizzbuzz.php
@@ -1,6 +1,18 @@
 <?php
 
-for ($i = 1; $i <= 100 ; $i++) { 
+$pos = strpos($argv[0], '.');
+$program = substr($argv[0], 0, $pos);
+$fileType = substr($argv[0], $pos + 1);
+
+echo "You have run $program!\n";
+echo "It is a $fileType file!\n\n";
+
+fwrite(STDOUT, "What is your low number? ");
+$rangeLow = rtrim(fgets(STDIN));
+fwrite(STDOUT, "What is your high number?");
+$rangeHigh = rtrim(fgets(STDIN));
+
+for ($i = $rangeLow; $i <= $rangeHigh; $i++) { 
 	if ($i % 3 == 0) {
 		if ($i % 5 == 0) {
 			echo "FizzBuzz\n";
