d01228e4f449efcec14a93bbd634b94159daba8b
xSHAFTx <cameron@thenearsky.com>
2014-02-11 11:19:02 -0600

    added return to functions

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -1,32 +1,32 @@
 <?php
 
 function add($a, $b) {
-    echo ($a + $b) . "\n";
+    return ($a + $b) . "\n";
 }
 
 function subtract($a, $b) {
-    echo ($a - $b) . "\n";
+    return ($a - $b) . "\n";
 }
 
 function multiply($a, $b) {
-    echo ($a * $b) . "\n";
+    return ($a * $b) . "\n";
 }
 
 function divide($a, $b) {
-    echo ($a / $b) . "\n";
+    return ($a / $b) . "\n";
 }
 
 function modulus($a, $b) {
-    echo ($a % $b) . "\n";
+    return ($a % $b) . "\n";
 }
 
 $first = 10;
 $second = 2;
 
-add($first, $second);
-subtract($first, $second);
-multiply($first, $second);
-divide($first, $second);
-modulus($first, $second);
+echo add($first, $second);
+echo subtract($first, $second);
+echo multiply($first, $second);
+echo divide($first, $second);
+echo modulus($first, $second);
 
 ?>
