52fabfdefdf09152e9f4d289d445eb7c000629ee
xSHAFTx <cameron@thenearsky.com>
2014-02-11 11:53:22 -0600

    added new functions

--- php/arithmetic2.php
+++ php/arithmetic2.php
@@ -25,9 +25,55 @@ function add2() {
 	$numList = func_get_args();
 	$add = 0;
 	for ($i = 0; $i < $numArgs; $i++) { 
-		$add += $numList[$i];
+		if ($i == 0) {
+			$add = $numList[$i];
+		} else {
+			$add += $numList[$i];
+		}
 	}
 	return $add . "\n";
+}
+
+function subtract2() {
+	$numArgs = func_num_args();
+	$numList = func_get_args();
+	$subtract = 0;
+	for ($i = 0; $i < $numArgs; $i++) { 
+		if ($i == 0) {
+			$subtract = $numList[$i];
+		} else {
+			$subtract -= $numList[$i];
+		}
+	}
+	return $subtract . "\n";
+}
+
+function mulitply2() {
+	$numArgs = func_num_args();
+	$numList = func_get_args();
+	$mulitply = 0;
+	for ($i = 0; $i < $numArgs; $i++) { 
+		if ($i == 0) {
+			$mulitply = $numList[$i];
+		} else {
+			$mulitply *= $numList[$i];
+		}
+	}
+	return $mulitply . "\n";
+}
+
+function divide2() {
+	$numArgs = func_num_args();
+	$numList = func_get_args();
+	$divide = 0;
+	for ($i = 0; $i < $numArgs; $i++) { 
+		if ($i == 0) {
+			$divide = $numList[$i];
+		} else {
+			$divide /= $numList[$i];
+		}
+	}
+	return $divide . "\n";
 }
 
 $first = 10;
@@ -38,6 +84,9 @@ $second = 2;
 // echo multiply($first, $second);
 // echo divide($first, $second);
 // echo modulus($first, $second);
-echo add2($first, $second, 8, 10, 13);
+echo add2($first, $second, 13, 10, 8);
+echo subtract2($first, $second, 13, 10, 8);
+echo mulitply2($first, $second, 13, 10, 8);
+echo divide2($first, $second, 13, 10, 8);
 
 ?>
