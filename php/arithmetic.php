300b61c16bf5a4d5332e63e1dafe38000eff8c90
xSHAFTx <cameron@thenearsky.com>
2014-02-11 10:56:59 -0600

    Functions I Exercise 1 Step 1

---
+++ php/arithmetic.php
@@ -0,0 +1,20 @@
+<?php
+
+function add($a, $b) {
+    echo $a + $b;
+}
+
+function subtract($a, $b) {
+    echo $a - $b;
+}
+
+function multiply($a, $b) {
+    echo $a * $b;
+}
+
+function divide($a, $b) {
+    echo $a / $b;
+}
+
+
+?>
