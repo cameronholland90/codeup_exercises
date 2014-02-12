5f1e23f3a99e473b87dc7086315642c8f316a5f2
xSHAFTx <cameron@thenearsky.com>
2014-02-11 19:15:52 -0600

    Functions I Exercise 5

--- codeup_todo_list/todo.php
+++ codeup_todo_list/todo.php
@@ -3,35 +3,57 @@
 // Create array to hold list of todo items
 $items = array();
 
+// List array items formatted for CLI
+function list_items($list)
+{
+    $output = "";
+    // Iterate through list items
+    foreach ($list as $key => $item) {
+        // Display each item and a newline
+        $key2 = $key + 1;
+        $output = $output . "[" . $key2 . "] item $key2 - {$item}\n";
+    }
+
+    return $output;
+}
+
+// Get STDIN, strip whitespace and newlines, 
+// and convert to uppercase if $upper is true
+function get_input($upper = FALSE) 
+{
+    $userInput = trim(fgets(STDIN));
+    if (ctype_lower($userInput) && $upper === TRUE) {
+        return strtoupper($userInput);
+    } else {
+        return $userInput;
+    }
+
+    // Return filtered STDIN input
+}
+
 // The loop!
 do {
-    // Iterate through list items
-    foreach ($items as $key => $item) {
-        // Display each item and a newline
-        //var_dump($key);
-        $key2 = $key + 1;
-        echo "[" . $key2 . "] {$item}\n";
-        //var_dump($key);
-    }
+    
+    echo list_items($items);
 
     // Show the menu options
     echo '(N)ew item, (R)emove item, (Q)uit : ';
 
     // Get the input from user
     // Use trim() to remove whitespace and newlines
-    $input = strtoupper(trim(fgets(STDIN)));
+    $input = get_input(TRUE);
     
     // Check for actionable input
     if ($input == 'N') {
         // Ask for entry
         echo 'Enter item: ';
         // Add entry to list array
-        $items[] = trim(fgets(STDIN));
+        $items[] = get_input();
     } elseif ($input == 'R') {
         // Remove which item?
         echo 'Enter item number to remove: ';
         // Get array key
-        $key = trim(fgets(STDIN));
+        $key = get_input();
         // Remove from array
         $key2 = $key - 1;
         unset($items[$key2]);
