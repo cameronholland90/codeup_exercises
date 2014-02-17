fd7c0738dc606dfa26b5a3af333d06044ccdfa67
cameronholland90 <cameron@thenearsky.com>
2014-02-17 14:29:47 -0600

    morsecode to english translator without arrays

--- codeup challenges/morsecode2.php
+++ codeup challenges/morsecode2.php
@@ -1,5 +1,23 @@
 <?php
 
+function get_input($upper = false) {
+    // Return filtered STDIN input
+    if($upper == TRUE) {
+        return strtoupper(trim(fgets(STDIN)));
+    } else {
+        return trim(fgets(STDIN));
+    }
+}
 
+function openFile() {
+    $filename = get_input();
+    $handle = fopen($filename, "r");
+    $contents = fread($handle, filesize($filename));
+    fclose($handle);
+    return $contents;
+}
+
+echo "What morsecode file would you like to open? ";
+$morsecode = openFile();
 
 ?>
