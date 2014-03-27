3bcf9a5e50070ec28ba3f68e10586594ff19447d
cameronholland90 <cameron@thenearsky.com>
2014-03-27 09:17:17 -0500

    static example exercise

---
+++ php/static.php
@@ -0,0 +1,19 @@
+<?php
+
+class Hello {
+	public static $defaultMessage = "Hello, World!";
+
+
+}
+
+echo Hello::$defaultMessage . PHP_EOL;
+
+// $h1 = new Hello();
+// $h2 = new Hello();
+
+// $h1->defaultMessage = "Hello, Codeup!";
+
+// echo $h1->defaultMessage . PHP_EOL;
+// echo $h2->defaultMessage . PHP_EOL;
+
+?>
